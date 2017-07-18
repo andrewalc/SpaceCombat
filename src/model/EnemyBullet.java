213ac0c669c7524b403bfbc8488e7aa7d9eb9a6f
andrewalc <andrewalc98@gmail.com>
2017-07-17 20:11:39 -0400

    EnemyBullet clean up and JavaDoc.

--- src/model/EnemyBullet.java
+++ src/model/EnemyBullet.java
@@ -4,25 +4,28 @@ package model;
 import java.awt.Point;
 
 /**
- * Class representing an Enemy's Bullets.
+ * Class that represents a bullet that EnemyCrafts shoot.
  */
 public class EnemyBullet extends AProjectile {
-    public static final int ENEMY_BULLET_HEIGHT = 4;
-    public static final int ENEMY_BULLET_WIDTH = 10;
-    private static final int ENEMY_BULLET_SPEED = 17;
-    /**
-     * Constructor for an enemy bullet. Requires a point for the bullet to spawn at.
-     * @param spawnPoint The point where this bullet should spawn.
-     */
-    public EnemyBullet(Point spawnPoint){
-        this.position = spawnPoint;
-        setVelocity(new Point(-ENEMY_BULLET_SPEED, 0));
-    }
-    /**
-     * Method call that moves the bullet.
-     */
-    public void move() {
-        this.position.x += this.getVelocity().getX();
-        this.checkVisibility();
-    }
+
+  // Constants
+  public static final int ENEMY_BULLET_HEIGHT = 4;
+  public static final int ENEMY_BULLET_WIDTH = 10;
+  private static final int ENEMY_BULLET_SPEED = 17;
+
+  /**
+   * Constructor for an enemy bullet. Requires a Point representing the bullet's spawn point.
+   *
+   * @param spawnPoint Point where this bullet should spawn.
+   */
+  public EnemyBullet(Point spawnPoint) {
+    this.position = spawnPoint;
+    setVelocity(new Point(-ENEMY_BULLET_SPEED, 0));
+  }
+
+  @Override
+  public void move() {
+    this.position.x += this.getVelocity().getX();
+    this.checkVisibility();
+  }
 }
